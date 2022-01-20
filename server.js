844cfa4b5d4db6d5e3775a23fe8b4735713e96eb
John Saville <johnasaville@gmail.com>
2022-01-20 11:33:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,11 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-app.listen(PORT, function () {
-  // console.log(`Started on http://localhost:${PORT}`);
+// app.listen(PORT, function () {
+//   // console.log(`Started on http://localhost:${PORT}`);
+// });
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
