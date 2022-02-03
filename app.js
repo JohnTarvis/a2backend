40286c90d5a73b47bf826012f569565b3287f3cf
John Saville <johnasaville@gmail.com>
2022-02-03 04:27:44 -0500

    another attempt at uploads

--- app.js
+++ app.js
@@ -16,6 +16,48 @@ const morgan = require("morgan");
 
 const app = express();
 
+///////////////////////////////////////////////////////////////////
+
+var multer = require('multer');
+var AWS = require('aws-sdk');
+
+var accessKeyId =  process.env.S3_ACCESS_KEY_ID;
+var secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
+
+AWS.config.update({
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey
+});
+
+var s3 = new AWS.S3();
+
+app.use(multer({ // https://github.com/expressjs/multer
+  dest: './', 
+  limits : { fileSize:100000 },
+  rename: function (fieldname, filename) {
+    return filename.replace(/\W+/g, '-').toLowerCase();
+  },
+  onFileUploadData: function (file, data, req, res) {
+    // file : { fieldname, originalname, name, encoding, mimetype, path, extension, size, truncated, buffer }
+    var params = {
+      Bucket: 'a2uploads',
+      Key: file.name,
+      Body: data
+    };
+
+    s3.putObject(params, function (perr, pres) {
+      if (perr) {
+        console.log("Error uploading data: ", perr);
+      } else {
+        console.log("Successfully uploaded data to myBucket/myKey");
+      }
+    });
+  }
+}));
+
+///////////////////////////////////////////////////////////////////
+
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
@@ -47,5 +89,7 @@ app.use(function (err, req, res, next) {
     error: { message, status },
   });
 });
+
+
 
 module.exports = app;
--- routes/post.js
+++ routes/post.js
@@ -41,51 +41,51 @@ router.get("/", async function (req, res, next) {
 
 //////////////////////////////////////////////////////////////////////////////////////CREATE POST
 
-var multer = require('multer');
-var AWS = require('aws-sdk');
+// var multer = require('multer');
+// var AWS = require('aws-sdk');
 
-var accessKeyId =  process.env.S3_ACCESS_KEY_ID;
-var secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
+// var accessKeyId =  process.env.S3_ACCESS_KEY_ID;
+// var secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
 
-AWS.config.update({
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKey
-});
+// AWS.config.update({
+//     accessKeyId: accessKeyId,
+//     secretAccessKey: secretAccessKey
+// });
 
 
-var s3 = new AWS.S3();
+// var s3 = new AWS.S3();
 
-app.use(multer({ // https://github.com/expressjs/multer
-  dest: './public/uploads/', 
-  limits : { fileSize:100000 },
-  rename: function (fieldname, filename) {
-    return filename.replace(/\W+/g, '-').toLowerCase();
-  },
-  onFileUploadData: function (file, data, req, res) {
-    // file : { fieldname, originalname, name, encoding, mimetype, path, extension, size, truncated, buffer }
-    var params = {
-      Bucket: 'a2uploads',
-      Key: file.name,
-      Body: data
-    };
+// app.use(multer({ // https://github.com/expressjs/multer
+//   dest: './public/uploads/', 
+//   limits : { fileSize:100000 },
+//   rename: function (fieldname, filename) {
+//     return filename.replace(/\W+/g, '-').toLowerCase();
+//   },
+//   onFileUploadData: function (file, data, req, res) {
+//     // file : { fieldname, originalname, name, encoding, mimetype, path, extension, size, truncated, buffer }
+//     var params = {
+//       Bucket: 'a2uploads',
+//       Key: file.name,
+//       Body: data
+//     };
 
-    s3.putObject(params, function (perr, pres) {
-      if (perr) {
-        console.log("Error uploading data: ", perr);
-      } else {
-        console.log("Successfully uploaded data to myBucket/myKey");
-      }
-    });
-  }
-}));
+//     s3.putObject(params, function (perr, pres) {
+//       if (perr) {
+//         console.log("Error uploading data: ", perr);
+//       } else {
+//         console.log("Successfully uploaded data to myBucket/myKey");
+//       }
+//     });
+//   }
+// }));
 
-app.post('/upload', function(req, res){
-    if(req.files.image !== undefined){ // `image` is the field name from your form
-        res.redirect("/uploads"); // success
-    }else{
-        res.send("error, no file chosen");
-    }
-});
+// app.post('/upload', function(req, res){
+//     if(req.files.image !== undefined){ // `image` is the field name from your form
+//         res.redirect("/uploads"); // success
+//     }else{
+//         res.send("error, no file chosen");
+//     }
+// });
 
 router.post("/", async function (req, res, next) {
   try {
@@ -94,9 +94,7 @@ router.post("/", async function (req, res, next) {
       const errs = validator.errors.map(e => e.stack);
       throw new BadRequestError(errs);
     }
-
     
-
     const newPost = await Post.create(req.body);
     return res.status(201).json({ newPost });
   } catch (err) {
