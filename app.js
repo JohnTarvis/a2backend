bc73139ca7803e59fe0c11031d9b9194baaec6cf
John Saville <johnasaville@gmail.com>
2022-02-03 15:42:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,38 +18,37 @@ const app = express();
 
 ///////////////////////////////////////////////////////////////////
 
-var multer = require('multer');
-var AWS = require('aws-sdk');
-
-var accessKeyId =  process.env.S3_ACCESS_KEY_ID;
-var secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
-AWS.config.update({
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKey
-});
-var s3 = new AWS.S3();
-app.use(multer({ // https://github.com/expressjs/multer
-  dest: '/post', 
-  limits : { fileSize:100000 },
-  rename: function (fieldname, filename) {
-    return filename.replace(/\W+/g, '-').toLowerCase();
-  },
-  onFileUploadData: function (file, data, req, res) {
-    // file : { fieldname, originalname, name, encoding, mimetype, path, extension, size, truncated, buffer }
-    var params = {
-      Bucket: 'a2uploads',
-      Key: file.name,
-      Body: data
-    };
-    s3.putObject(params, function (perr, pres) {
-      if (perr) {
-        console.log("Error uploading data: ", perr);
-      } else {
-        console.log("Successfully uploaded data to myBucket/myKey");
-      }
-    });
-  }
-}));
+// var multer = require('multer');
+// var AWS = require('aws-sdk');
+// var accessKeyId =  process.env.S3_ACCESS_KEY_ID;
+// var secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
+// AWS.config.update({
+//     accessKeyId: accessKeyId,
+//     secretAccessKey: secretAccessKey
+// });
+// var s3 = new AWS.S3();
+// app.use(multer({ // https://github.com/expressjs/multer
+//   dest: '/post', 
+//   limits : { fileSize:100000 },
+//   rename: function (fieldname, filename) {
+//     return filename.replace(/\W+/g, '-').toLowerCase();
+//   },
+//   onFileUploadData: function (file, data, req, res) {
+//     // file : { fieldname, originalname, name, encoding, mimetype, path, extension, size, truncated, buffer }
+//     var params = {
+//       Bucket: 'a2uploads',
+//       Key: file.name,
+//       Body: data
+//     };
+//     s3.putObject(params, function (perr, pres) {
+//       if (perr) {
+//         console.log("Error uploading data: ", perr);
+//       } else {
+//         console.log("Successfully uploaded data to myBucket/myKey");
+//       }
+//     });
+//   }
+// }));
 
 ///////////////////////////////////////////////////////////////////
 
