992ccad7390edc8f051009abd6999ef79a8c154d
John Saville <johnasaville@gmail.com>
2022-02-05 02:40:37 -0500

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -13,46 +13,28 @@ const postUpdateSchema = require("../schemas/postUpdate.json");
 const postSearchSchema = require("../schemas/postSearch.json");
 const router = express.Router({ mergeParams: true });
 
-///////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////
 
-const aws = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
-aws.config.update({
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    accessKeyId: process.env.S3_ACCESS_KEY_ID,
-    region: 'us-west-1'
-});
-const s3 = new aws.S3();
-var upload = multer({
-  storage: multerS3({
-      s3: s3,
-      bucket: 'a2uploads',
-      key: function (req, file, cb) {
-          console.log(file);
-          cb(null, file.originalname); //use Date.now() for unique file keys
-      }
-  })
-});
-app.post("/",upload.array('upl',1), async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, postNewSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
-    }
-    
-    const newPost = await Post.create(req.body);
-    return res.status(201).json({ newPost });
-  } catch (err) {
-    return next(err);
-  }
-});
-
-
-///////////////////////////////////////////////////////////////////////////////////////
-
-// router.post("/", async function (req, res, next) {
+// const aws = require('aws-sdk');
+// const multer = require('multer');
+// const multerS3 = require('multer-s3');
+// aws.config.update({
+//     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+//     accessKeyId: process.env.S3_ACCESS_KEY_ID,
+//     region: 'us-west-1'
+// });
+// const s3 = new aws.S3();
+// var upload = multer({
+//   storage: multerS3({
+//       s3: s3,
+//       bucket: 'a2uploads',
+//       key: function (req, file, cb) {
+//           console.log(file);
+//           cb(null, file.originalname); //use Date.now() for unique file keys
+//       }
+//   })
+// });
+// app.post("/",upload.array('upl',1), async function (req, res, next) {
 //   try {
 //     const validator = jsonschema.validate(req.body, postNewSchema);
 //     if (!validator.valid) {
@@ -66,6 +48,24 @@ app.post("/",upload.array('upl',1), async function (req, res, next) {
 //     return next(err);
 //   }
 // });
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+router.post("/", async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, postNewSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+    
+    const newPost = await Post.create(req.body);
+    return res.status(201).json({ newPost });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 //////////////////////////////////////////////////////////////////////////////////////GET POST
 
