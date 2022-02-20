99ea234931476041df454a0858c37b967e9fc4a3
John Saville <johnasaville@gmail.com>
2022-02-20 08:23:14 -0500

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -45,9 +45,9 @@ var upload = multer({
 router.post("/",upload.array('upload',1), async function (req, res, next) {
   try {
 
-    console.log('upload==================',upload);
-    console.log('reqbody=================',req.body)
-    console.log('reqbodyfile=================',req.body.file)
+    // console.log('upload==================',upload);
+    // console.log('reqbody=================',req.body)
+    console.log('reqbodyfile=================',req.body.file);
 
 
     const validator = jsonschema.validate(req.body, postNewSchema);
