59ad38eea711dc15f24886e4a8ab7c60b5624c0e
John Saville <53898479+JohnTarvis@users.noreply.github.com>
2022-05-26 17:41:57 -0600

    Update post.js

--- models/post.js
+++ models/post.js
@@ -122,60 +122,60 @@ class Post {
 
   ////////////////////////////////////////////////////////////////////////GET WITH
 
-  // static async getWith(searchFilters = {}){
-  //   let query = `SELECT poster_id,
-  //                       poster_handle,
-  //                       post_date,
-  //                       post_body,
-  //                       post_subject,
-  //                       post_tags,
-  //                       admin_post,
-  //                       image,
-  //                       id,
-  //                       reply_to
-  //                FROM posts`;
+  static async getWith(searchFilters = {}){
+    let query = `SELECT poster_id,
+                        poster_handle,
+                        post_date,
+                        post_body,
+                        post_subject,
+                        post_tags,
+                        admin_post,
+                        image,
+                        id,
+                        reply_to
+                 FROM posts`;
 
-  //   const whereExpressions = [];
-  //   const whereValues = [];
-  //   const filterNames = Object.keys(searchFilters);
+    const whereExpressions = [];
+    const whereValues = [];
+    const filterNames = Object.keys(searchFilters);
 
-  //   if (searchFilters){
-  //     for(const filterName of filterNames){
-  //       const whereValue = searchFilters[filterName];
-  //       whereValues.push(`${whereValue}`);
-  //       whereExpressions.push(`${filterName} = $${whereValues.length}`);
-  //     }
-  //     if(whereExpressions.length > 0){
-  //       query += " WHERE " + whereExpressions.join(" AND ");
-  //     }
-  //   }
+    if (searchFilters){
+      for(const filterName of filterNames){
+        const whereValue = searchFilters[filterName];
+        whereValues.push(`${whereValue}`);
+        whereExpressions.push(`${filterName} = $${whereValues.length}`);
+      }
+      if(whereExpressions.length > 0){
+        query += " WHERE " + whereExpressions.join(" AND ");
+      }
+    }
 
-  //   query += " ORDER BY id ";
+    query += " ORDER BY id ";
 
-  //   const results = await db.query(query,whereValues);
-  //   return results.rows;
+    const results = await db.query(query,whereValues);
+    return results.rows;
     
-  // }
+  }
 
-  // static async get(handle) {
-  //   const postRes = await db.query(
-  //         `SELECT poster_id,
-  //                 poster_handle,
-  //                 post_date,
-  //                 post_body,
-  //                 post_subject,
-  //                 post_tags,
-  //                 admin_post,
-  //                 image,
-  //                 id
-  //          FROM posts
-  //          WHERE handle = $1`,
-  //       [handle]);
-  //   const post = postRes.rows[0];
-  //   if (!post) throw new NotFoundError(`No post: ${handle}`);
-  //   post.posts = postRes.rows;
-  //   return post;
-  // }
+  static async get(handle) {
+    const postRes = await db.query(
+          `SELECT poster_id,
+                  poster_handle,
+                  post_date,
+                  post_body,
+                  post_subject,
+                  post_tags,
+                  admin_post,
+                  image,
+                  id
+           FROM posts
+           WHERE handle = $1`,
+        [handle]);
+    const post = postRes.rows[0];
+    if (!post) throw new NotFoundError(`No post: ${handle}`);
+    post.posts = postRes.rows;
+    return post;
+  }
 
   ////////////////////////////////////////////////////////////////////////REMOVE
 
